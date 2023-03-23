d6ed2e89645794218bf38db7cea9e5745139d426
Serhii Buria <sergeiburya@gmail.com>
2023-03-23 16:03:00 +0200

    project initialization, start dependencies added

---
+++ src/main/java/team/project/foodsparks/FoodSparksApplication.java
@@ -0,0 +1,13 @@
+package team.project.foodsparks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodSparksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodSparksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team/project/foodsparks/FoodSparksApplicationTests.java
@@ -0,0 +1,13 @@
+package team.project.foodsparks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodSparksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
