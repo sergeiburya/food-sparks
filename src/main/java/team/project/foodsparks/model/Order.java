e9b8b1f5df1655b75fef2eaedca3d977ad3f9053
Serhii Buria <sergeiburya@gmail.com>
2023-03-31 21:21:44 +0300

    sdded model class Order

--- src/main/java/team/project/foodsparks/model/Order.java
+++ src/main/java/team/project/foodsparks/model/Order.java
@@ -1,37 +1,42 @@
 package team.project.foodsparks.model;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Objects;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
-import javax.persistence.MapsId;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "shopping_carts")
-public class ShoppingCart {
+@Entity
+@Table(name = "orders")
+public class Order {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @OneToMany
-    @JoinTable(name = "shopping_carts_ingredients",
-            joinColumns = @JoinColumn(name = "shopping_cart_id"),
+    @JoinTable(name = "orders_ingredients",
+            joinColumns = @JoinColumn(name = "order_id"),
             inverseJoinColumns = @JoinColumn(name = "ingredient_id"))
     private List<Ingredient> ingredients;
-    @MapsId
-    @OneToOne
-    @JoinColumn(name = "id")
+    @Column(name = "order_time")
+    private LocalDateTime orderTime;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
     private User user;
 
     @Override
@@ -42,23 +47,25 @@ public class ShoppingCart {
         if (obj == null || getClass() != getClass()) {
             return false;
         }
-        ShoppingCart that = (ShoppingCart) obj;
+        Order order = (Order) obj;
         return getId() != null
-                && Objects.equals(getId(), that.getId())
-                && Objects.equals(getUser(), that.getUser())
-                && Objects.equals(getIngredients(), that.getIngredients());
+                && Objects.equals(getId(), order.getId())
+                && Objects.equals(getIngredients(), order.getIngredients())
+                && Objects.equals(getUser(), order.getUser())
+                && Objects.equals(getOrderTime(), order.getOrderTime());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, ingredients, user);
+        return Objects.hash(id, ingredients, orderTime, user);
     }
 
     @Override
     public String toString() {
-        return "ShoppingCart{"
+        return "Order{"
                 + "id=" + id
-                + ", tickets=" + ingredients
+                + ", ingredients=" + ingredients
+                + ", orderTime=" + orderTime
                 + ", user=" + user + '}';
     }
 }
