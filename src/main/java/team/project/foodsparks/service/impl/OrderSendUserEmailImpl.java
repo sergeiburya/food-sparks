8d4700656f514baa9c37f31819a46c3fb1fb1558
Serhii Buria <sergeiburya@gmail.com>
2023-04-14 15:19:18 +0300

    update OrderSendUserEmailImpl

--- src/main/java/team/project/foodsparks/service/impl/OrderSendUserEmailImpl.java
+++ src/main/java/team/project/foodsparks/service/impl/OrderSendUserEmailImpl.java
@@ -1,10 +1,9 @@
 package team.project.foodsparks.service.impl;
 
-import com.lowagie.text.Document;
-import com.lowagie.text.DocumentException;
-import com.lowagie.text.Paragraph;
+import com.lowagie.text.*;
 import com.lowagie.text.pdf.PdfWriter;
 import java.io.ByteArrayOutputStream;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
 import java.util.Properties;
 import javax.mail.Authenticator;
@@ -20,6 +19,7 @@ import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import team.project.foodsparks.exeption.DataProcessingException;
 import team.project.foodsparks.model.Address;
 import team.project.foodsparks.model.Order;
 import team.project.foodsparks.model.Product;
@@ -43,26 +43,66 @@ public class OrderSendUserEmailImpl implements OrderSendUserEmail {
         Document document = new Document();
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         PdfWriter.getInstance(document, baos);
+
         Address userAddress = addressService.findByUser(user).orElseThrow(
                 () -> new RuntimeException("Address with user " + user + "not found"));
         document.open();
-        document.add(new Paragraph("User Region: " + userAddress.getRegion()));
-        document.add(new Paragraph("User Town: " + userAddress.getTown()));
-        document.add(new Paragraph("User Street: " + userAddress.getStreet()));
-        document.add(new Paragraph("User Build: " + userAddress.getBuild()));
-        document.add(new Paragraph("User Apartment: " + userAddress.getApartment()));
-        document.add(new Paragraph("Order Number: " + order.getId()));
-        document.add(new Paragraph("Order Time: " + order.getOrderTime()));
-        document.add(new Paragraph("User Name: " + user.getFirstName() + user.getLastName()));
-        document.add(new Paragraph("User Phone: " + user.getPhone()));
-
-        document.add(new Paragraph("Order Items:"));
-
+        document.setMargins(72, 36, 36, 36);
+        Paragraph titleParagraph = new Paragraph("Thank you for your order.\n" +
+                "Here are the details of your order.", FontFactory.getFont(FontFactory.TIMES, 12));
+        titleParagraph.setAlignment(Element.ALIGN_CENTER);
+        titleParagraph.setLeading(20f);
+        document.add(titleParagraph);
+        Paragraph orderNumParagraph = new Paragraph("Order Number: FS000" + order.getId(),
+                FontFactory.getFont(FontFactory.COURIER_BOLDOBLIQUE));
+        orderNumParagraph.setAlignment(Element.ALIGN_CENTER);
+        orderNumParagraph.setLeading(30f);
+        document.add(orderNumParagraph);
+        Paragraph dateParagraph = new Paragraph("Orders Time: " + order.getOrderTime()
+                .format(DateTimeFormatter.ofPattern("dd/MM/yyyy - HH:mm")));
+        dateParagraph.setAlignment(Element.ALIGN_RIGHT);
+        dateParagraph.setLeading(20f);
+        document.add(dateParagraph);
+        Paragraph adrressParagraph = new Paragraph("Users Address: " + userAddress.getRegion() + ", "
+                + userAddress.getTown()+ ", "
+                + userAddress.getStreet()+ ", "
+                + userAddress.getBuild()+ ", "
+                + userAddress.getApartment());
+        adrressParagraph.setAlignment(Element.ALIGN_LEFT);
+        adrressParagraph.setLeading(20f);
+        document.add(adrressParagraph);
+        Paragraph userParagraph = new Paragraph("Users Name: " + user.getFirstName()
+                + user.getLastName(), FontFactory.getFont(FontFactory.COURIER));
+        userParagraph.setAlignment(Element.ALIGN_LEFT);
+        userParagraph.setLeading(20f);
+        document.add(userParagraph);
+        Paragraph phoneParagraph = new Paragraph("Users Phone: " + user.getPhone());
+        phoneParagraph.setLeading(20f);
+        phoneParagraph.setAlignment(Element.ALIGN_LEFT);
+        document.add(phoneParagraph);
+        Paragraph orderParagraph = new Paragraph("Order Items:", FontFactory.getFont(FontFactory.HELVETICA));
+        orderParagraph.setAlignment(Element.ALIGN_LEFT);
+        orderParagraph.setLeading(20f);
+        document.add(orderParagraph);
         Map<Product, Integer> productAmount = order.getProductAmount();
         for (Map.Entry<Product, Integer> entry : productAmount.entrySet()) {
-            document.add(new Paragraph(entry.getKey().getName() + " - " + entry.getValue()));
+            Paragraph itemParagraph = new Paragraph("Product Name: " + entry.getKey().getName().toUpperCase()
+                    + " - " + entry.getValue());
+            itemParagraph.setLeading(20f);
+            itemParagraph.setAlignment(Element.ALIGN_LEFT);
+            document.add(itemParagraph);
         }
-        document.add(new Paragraph("Total: " + order.getSum()));
+        Paragraph totalSumparagraph = new Paragraph("Total Summa: " + order.getSum());
+        totalSumparagraph.setAlignment(Element.ALIGN_RIGHT);
+        document.add(totalSumparagraph);
+        Paragraph endParagraph = new Paragraph("We will be glad to welcome "
+                + "you again in our store.\n"
+                + "If you have any questions, "
+                + "you can contact us at: foodsparksmail@gmail.com",
+                FontFactory.getFont(FontFactory.TIMES, 12));
+        endParagraph.setSpacingBefore(60f);
+        endParagraph.setAlignment(Element.ALIGN_CENTER);
+        document.add(endParagraph);
         document.close();
 
         byte[] pdfBytes = baos.toByteArray();
@@ -73,7 +113,7 @@ public class OrderSendUserEmailImpl implements OrderSendUserEmail {
         props.put("mail.smtp.port", "587");
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.auth","true");
+        props.put("mail.smtp.auth", "true");
 
         Session session = Session.getInstance(props, new Authenticator() {
             @Override
@@ -88,7 +128,7 @@ public class OrderSendUserEmailImpl implements OrderSendUserEmail {
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(userEmail));
             message.setSubject("Your order details");
         } catch (MessagingException e) {
-            throw new RuntimeException(e);
+            throw new DataProcessingException("A message with an order cant be sent.",e);
         }
 
         Multipart multipart = new MimeMultipart();
@@ -100,7 +140,7 @@ public class OrderSendUserEmailImpl implements OrderSendUserEmail {
             message.setContent(multipart);
             Transport.send(message);
         } catch (MessagingException e) {
-            throw new RuntimeException(e);
+            throw new DataProcessingException("A message with an order cant be sent.",e);
         }
     }
 }
