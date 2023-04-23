6bbe46305fab52bd4c70b11a9d5bac3a9c08c047
Serhii Buria <sergeiburya@gmail.com>
2023-04-23 17:30:06 +0300

    update method createAndSendPdfOrderForUser

--- src/main/java/team/project/foodsparks/service/impl/OrderServiceImpl.java
+++ src/main/java/team/project/foodsparks/service/impl/OrderServiceImpl.java
@@ -4,10 +4,11 @@ import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
 import com.lowagie.text.Element;
 import com.lowagie.text.Font;
-import com.lowagie.text.FontFactory;
+import com.lowagie.text.Image;
 import com.lowagie.text.Paragraph;
 import com.lowagie.text.pdf.BaseFont;
 import com.lowagie.text.pdf.PdfWriter;
+import com.lowagie.text.pdf.draw.LineSeparator;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.math.BigDecimal;
@@ -36,6 +37,7 @@ import team.project.foodsparks.util.ProductAmountConverter;
 @Service
 public class OrderServiceImpl implements OrderService {
     private static final String FONT_PATH = "/fonts/arialuni.ttf";
+    private static final String LOGO_PATH = "src/main/resources/static/img/logo.jpg";
     private final OrderRepository orderRepository;
     private final ShoppingCartService shoppingCartService;
     private final EmailService emailService;
@@ -103,45 +105,52 @@ public class OrderServiceImpl implements OrderService {
         PdfWriter writer = PdfWriter.getInstance(document, baos);
         writer.setPdfVersion(PdfWriter.VERSION_1_7);
         document.open();
+        Image logoImage = Image.getInstance(LOGO_PATH);
+        logoImage.scaleAbsolute(250f, 20f);
+        logoImage.setIndentationLeft(135f);
+        Paragraph lineSeparator = new Paragraph();
+        lineSeparator.add(new LineSeparator());
+        lineSeparator.setLeading(10f);
+        document.setMargins(72, 36, 36, 36);
+        document.add(logoImage);
         BaseFont bf = BaseFont.createFont(FONT_PATH, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
         Font arialFont = new Font(bf, 12);
-
-        document.open();
-        document.setMargins(72, 36, 36, 36);
-        Paragraph titleParagraph = new Paragraph("Thank you for order.\n"
-                + "Here are the details of your order.",
-                FontFactory.getFont(FontFactory.TIMES, 12));
+        Paragraph titleParagraph = new Paragraph("Дякуємо за Ваше замовлення. "
+                + "Деталі замовлення.", arialFont);
         titleParagraph.setAlignment(Element.ALIGN_CENTER);
-        titleParagraph.setLeading(30f);
+        titleParagraph.setLeading(10f);
         document.add(titleParagraph);
-        Paragraph orderNumParagraph = new Paragraph("Order Number: FS000" + order.getId(),
-                FontFactory.getFont(FontFactory.TIMES_ROMAN, 16));
+        Paragraph orderNumParagraph = new Paragraph("Номер замовлення: \n FS000" + order.getId(),
+                arialFont);
         orderNumParagraph.setAlignment(Element.ALIGN_CENTER);
         orderNumParagraph.setLeading(30f);
         document.add(orderNumParagraph);
-        Paragraph dateParagraph = new Paragraph("Orders Time: " + order.getOrderTime()
+        document.add(lineSeparator);
+        Paragraph dateParagraph = new Paragraph("Дата та час замовлення: " + order.getOrderTime()
                 .format(DateTimeFormatter.ofPattern("dd/MM/yyyy - HH:mm")),
-                FontFactory.getFont(FontFactory.TIMES_ROMAN));
+                arialFont);
         dateParagraph.setAlignment(Element.ALIGN_RIGHT);
         dateParagraph.setLeading(30f);
         document.add(dateParagraph);
         DeliveryInformation deliveryInformation = order.getDeliveryInformation();
-        Paragraph adrressParagraph = new Paragraph("Delivery addresses: "
+        Paragraph adrressParagraph = new Paragraph("Адреса доставки: "
                 + deliveryInformation.getTown() + ", "
                 + deliveryInformation.getStreet() + ", "
                 + deliveryInformation.getBuild() + ", "
-                + deliveryInformation.getApartment(), FontFactory.getFont(FontFactory.TIMES_ROMAN));
+                + deliveryInformation.getApartment(), arialFont);
         adrressParagraph.setAlignment(Element.ALIGN_LEFT);
         adrressParagraph.setLeading(30f);
         document.add(adrressParagraph);
-        Paragraph userParagraph = new Paragraph("Users Name: " + deliveryInformation.getFirstName()
+        Paragraph userParagraph = new Paragraph("Ім'я користувача: "
+                + deliveryInformation.getFirstName()
                 + " " + deliveryInformation.getLastName(),
-                FontFactory.getFont(FontFactory.TIMES_ROMAN));
+                arialFont);
         userParagraph.setAlignment(Element.ALIGN_LEFT);
         userParagraph.setLeading(30f);
         document.add(userParagraph);
-        Paragraph phoneParagraph = new Paragraph("Users Phone: " + deliveryInformation.getPhone(),
-                FontFactory.getFont(FontFactory.TIMES_ROMAN));
+        Paragraph phoneParagraph = new Paragraph("Тел.: "
+                + deliveryInformation.getPhone(),
+                arialFont);
         phoneParagraph.setLeading(30f);
         phoneParagraph.setAlignment(Element.ALIGN_LEFT);
         document.add(phoneParagraph);
@@ -151,29 +160,45 @@ public class OrderServiceImpl implements OrderService {
         Paragraph deliveryDayParagraf = new Paragraph();
         deliveryDayParagraf.setLeading(30f);
         document.add(deliveryDayParagraf);
-        Paragraph orderParagraph = new Paragraph("Order Items:",
-                FontFactory.getFont(FontFactory.HELVETICA));
+        Paragraph orderParagraph = new Paragraph("Список товарів:",
+                arialFont);
         orderParagraph.setAlignment(Element.ALIGN_LEFT);
         orderParagraph.setLeading(40f);
         document.add(orderParagraph);
+        document.add(lineSeparator);
         Map<Product, Integer> productAmount = order.getProductAmount();
+        int numberItems = 1;
         for (Map.Entry<Product, Integer> entry : productAmount.entrySet()) {
-            Paragraph itemParagraph = new Paragraph("Product Name: "
-                    + entry.getKey().getName()
-                    + " - " + entry.getValue() + " - "
-                    + ProductAmountConverter.convertProductAmount(entry.getKey().getAmountInPackage()), arialFont);
+            Paragraph itemParagraph = new Paragraph(numberItems + ". Назва товару: "
+                    + entry.getKey().getName() + " - ("
+                    + ProductAmountConverter.convertProductAmount(
+                            entry.getKey().getAmountInPackage())
+                    + "/упак) - " + entry.getValue() + " шт. ", arialFont);
             itemParagraph.setLeading(30f);
             itemParagraph.setAlignment(Element.ALIGN_LEFT);
             document.add(itemParagraph);
+            numberItems++;
         }
-        Paragraph totalSumparagraph = new Paragraph("Total Summa: " + order.getSum());
+        document.add(lineSeparator);
+        Paragraph totalSumparagraph = new Paragraph("Сумма замовлення: "
+                + order.getSum(), arialFont);
+        totalSumparagraph.setLeading(20f);
         totalSumparagraph.setAlignment(Element.ALIGN_RIGHT);
         document.add(totalSumparagraph);
-        Paragraph endParagraph = new Paragraph("We will be glad to welcome "
-                + "you again in our store.\n"
-                + "If you have any questions, "
-                + "you can contact us at: foodsparksmail@gmail.com",
-                FontFactory.getFont(FontFactory.TIMES, 12));
+        Paragraph dateTimeDelivery = new Paragraph("Дата та час доставки: -"
+                + order.getDeliveryInformation().getDayOfDelivery() + " - "
+                + order.getDeliveryInformation().getTimeOfDelivery(), arialFont);
+        dateTimeDelivery.setLeading(20f);
+        document.add(dateTimeDelivery);
+        Paragraph commentParagraph = new Paragraph("Коментар до замовлення: - "
+                + order.getDeliveryInformation().getComment(), arialFont);
+        commentParagraph.setLeading(20f);
+        document.add(commentParagraph);
+
+        Paragraph endParagraph = new Paragraph("Будемо раді знову вітати Вас у нашому магазині. "
+                + "Якщо у вас виникнуть запитання, ви можете зв’язатися з нами за адресою: "
+                + "foodsparksmail@gmail.com",
+                arialFont);
         endParagraph.setSpacingBefore(80f);
         endParagraph.setAlignment(Element.ALIGN_CENTER);
         document.add(endParagraph);
